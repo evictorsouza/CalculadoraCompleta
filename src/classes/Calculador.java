0b138818a08539216ab882ebf7681beb6b0e6605
Edson Victor Souza <edson.victor563@gmail.com>
2024-08-09 02:41:15 -0300

    Encapsulamento Concluido

---
+++ src/classes/Calculador.java
@@ -0,0 +1,364 @@
+package classes;
+/**
+ *
+ * @author edson
+ */
+public class Calculador implements Calculadora{
+    //ATRIBUTOS
+    private double num;
+    private boolean somando;
+    private boolean subtraindo;
+    private boolean igual;
+    private boolean multiplicando;
+    private boolean dividindo;
+    
+    //METODOS PERSONALIZADOS
+    private void Somar(String valor){
+        if(!this.isIgual()){
+            if(this.num != 0){
+                this.num +=  Double.parseDouble(valor);
+            }else{
+                this.num = Double.parseDouble(valor);
+            }
+        }
+    }
+    
+    private void Subtrair(String valor){
+        if(!this.isIgual()){
+            if(this.num != 0){
+                this.num -=  Double.parseDouble(valor);
+            }else{
+                this.num = Double.parseDouble(valor);
+            }
+        }
+    }
+    
+    private void Multiplicar(String valor){
+        if(!this.isIgual()){
+            if(this.num != 0){
+                this.num *=  Double.parseDouble(valor);
+            }else{
+                this.num = Double.parseDouble(valor);
+            }
+        }
+    }
+    
+    private void Dividir(String valor){
+        if(!this.isIgual()){
+            if(this.num != 0){
+                this.num /=  Double.parseDouble(valor);
+            }else{
+                this.num = Double.parseDouble(valor);
+            }
+        }
+    }
+    
+    private void Raiz(String valor){
+        if(!this.isIgual()){
+            if(this.num >= 0){
+                this.num =  Math.sqrt(Double.parseDouble(valor));
+            }else{
+                this.num = 0;
+            }
+        }
+    }
+    
+    private void Porcentagem(String valor){
+        if(!this.isIgual()){
+                this.num =  Double.parseDouble(valor)/100;
+        }
+    }
+    
+    private String isInteiro(){
+        String inteiro;
+        if (this.getNum() % 1 == 0){
+               inteiro = Integer.toString((int) this.getNum());
+           }else{
+               inteiro = Double.toString(this.getNum());
+           }
+        return inteiro;
+    }
+    // CONSTRUTOR
+    public Calculador() {
+        this.setNum(0);
+        this.setSomando(false);
+        this.setSubtraindo(false);
+        this.setMultiplicando(false);
+        this.setDividindo(false);
+        this.setIgual(false);
+    }
+
+    //GETTERS E SETTERS
+    private double getNum() {
+        return this.num;
+    }
+
+    private void setNum(double num) {
+        this.num = num;
+    }
+
+    private boolean isSomando() {
+        return this.somando;
+    }
+
+    private void setSomando(boolean somando) {
+        this.somando = somando;
+    }
+
+    private boolean isSubtraindo() {
+        return this.subtraindo;
+    }
+
+    private void setSubtraindo(boolean subtraindo) {
+        this.subtraindo = subtraindo;
+    }
+
+    private boolean isIgual() {
+        return this.igual;
+    }
+
+    private void setIgual(boolean igual) {
+        this.igual = igual;
+    }
+
+    private boolean isMultiplicando() {
+        return multiplicando;
+    }
+
+    private void setMultiplicando(boolean multiplicando) {
+        this.multiplicando = multiplicando;
+    }
+
+    private boolean isDividindo() {
+        return dividindo;
+    }
+
+    private void setDividindo(boolean dividindo) {
+        this.dividindo = dividindo;
+    } 
+
+    //METODOS DA INTERFACE
+    @Override
+    public String botaoZero(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "0";
+        }else{
+            return valor += "0";
+        }
+    }
+
+    @Override
+    public String botaoUm(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "1";
+        }else{
+            return valor += "1";
+        }
+    }
+
+    @Override
+    public String botaoDois(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "2";
+        }else{
+            return valor += "2";
+        }
+    }
+
+    @Override
+    public String botaoTres(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "3";
+        }else{
+            return valor += "3";
+        }
+    }
+
+    @Override
+    public String botaoQuatro(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "4";
+        }else{
+            return valor += "4";
+        }
+    }
+
+    @Override
+    public String botaoCinco(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "5";
+        }else{
+            return valor += "5";
+        }
+    }
+
+    @Override
+    public String botaoSeis(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "6";
+        }else{
+            return valor += "6";
+        }
+    }
+
+    @Override
+    public String botaoSete(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "7";
+        }else{
+            return valor += "7";
+        }
+    }
+
+    @Override
+    public String botaoOito(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "8";
+        }else{
+            return valor += "8";
+        }
+    }
+
+    @Override
+    public String botaoNove(String texto) {
+        String valor = texto;
+        if(texto.equals("0")){
+            return valor = "9";
+        }else{
+            return valor += "9";
+        }
+    }
+
+    @Override
+    public String botaoPonto(String texto) {
+        String valor = texto;
+        if(!texto.contains(".")) valor += ".";
+        return valor;
+    }
+    @Override
+    public String botaoMaisMenos(String texto) {
+        if (Double.parseDouble(texto) % 1 == 0){
+            int n = Integer.parseInt(texto) * -1;
+            texto = Integer.toString(n);
+            return texto;
+        }else{
+            double n = Double.parseDouble(texto) *-1;
+            texto = Double.toString(n);
+            return texto;
+        }
+    }
+     
+    @Override
+    public String botaoSomar(String texto) {
+        this.setSomando(true);
+        this.setSubtraindo(false);
+        this.setMultiplicando(false);
+        this.setDividindo(false);
+        this.setIgual(false);
+        this.Somar(texto);
+        return "0";
+    }
+
+    @Override
+    public String botaoSubtrair(String texto) {
+        this.setSubtraindo(true);
+        this.setSomando(false);
+        this.setMultiplicando(false);
+        this.setDividindo(false);
+        this.setIgual(false);
+        this.Subtrair(texto);
+        return "0";
+    }
+
+    @Override
+    public String botaoMultiplicar(String texto) {
+        this.setMultiplicando(true);
+        this.setSomando(false);
+        this.setSubtraindo(false);
+        this.setDividindo(false);
+        this.setIgual(false);
+        this.Multiplicar(texto);
+        return "0";
+    }
+
+    @Override
+    public String botaoDividir(String texto) {
+        this.setDividindo(true);
+        this.setMultiplicando(false);
+        this.setSomando(false);
+        this.setSubtraindo(false);
+        this.setIgual(false);
+        this.Dividir(texto);
+        return "0";
+    }
+
+    @Override
+    public String botaoRaiz(String texto) {
+        this.setDividindo(false);
+        this.setMultiplicando(false);
+        this.setSomando(false);
+        this.setSubtraindo(false);
+        this.setIgual(false);
+        this.Raiz(texto);
+        texto = this.isInteiro();
+        this.setNum(0);
+        return texto;
+    }
+
+    @Override
+    public String botaoPorcentagem(String texto) {
+        this.setDividindo(false);
+        this.setMultiplicando(false);
+        this.setSomando(false);
+        this.setSubtraindo(false);
+        this.setIgual(false);
+        this.Porcentagem(texto);
+        texto = this.isInteiro();
+        this.setNum(0);
+        return texto;
+    }
+    
+    @Override
+    public String botaoIgual(String texto){
+        if(this.isSomando()){
+           this.Somar(texto);
+           texto = this.isInteiro();
+       }
+       if(this.isSubtraindo()){
+           this.Subtrair(texto);
+           texto = this.isInteiro();
+       }
+       if(this.isMultiplicando()){
+           this.Multiplicar(texto);
+           texto = this.isInteiro();
+       }
+       if (this.isDividindo()){
+           this.Dividir(texto);
+           texto = this.isInteiro();
+       }
+       this.setNum(0);
+       this.setSomando(false);
+       this.setSubtraindo(false);
+       this.setMultiplicando(false);
+       this.setDividindo(false);
+       this.setIgual(true);
+       return texto;
+    }
+
+    @Override
+    public String botaoClear() {
+        this.setNum(0);
+        return "0";
+    }
+    
+    
+}
---
+++ src/classes/Calculadora.java
@@ -0,0 +1,29 @@
+package classes;
+
+/**
+ *
+ * @author edson
+ */
+public interface Calculadora {
+    public abstract String botaoZero(String texto);
+    public abstract String botaoUm(String texto);
+    public abstract String botaoDois(String texto);
+    public abstract String botaoTres(String texto);
+    public abstract String botaoQuatro(String texto);
+    public abstract String botaoCinco(String texto);
+    public abstract String botaoSeis(String texto);
+    public abstract String botaoSete(String texto);
+    public abstract String botaoOito(String texto);
+    public abstract String botaoNove(String texto);
+    public abstract String botaoPonto(String texto);
+    public abstract String botaoMaisMenos(String texto);
+    public abstract String botaoSomar(String texto);
+    public abstract String botaoSubtrair(String texto);
+    public abstract String botaoMultiplicar(String texto);
+    public abstract String botaoDividir(String texto);
+    public abstract String botaoRaiz(String texto);
+    public abstract String botaoPorcentagem(String texto);
+    public abstract String botaoIgual(String texto);
+    public abstract String botaoClear();
+    
+}
